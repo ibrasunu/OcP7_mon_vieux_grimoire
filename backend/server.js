7782b999b5f402687f28343dc22550795f76f322
Ibrahima <ibrasunu@gmail.com>
2023-11-15 17:35:17 +0100

    server.js amelorized

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,54 @@
-const http = require('http');
+const http = require("http");
+const app = require("./app");
 
-const server = http.createServer((req, res) => {
-    res.end('Voilà la réponse du serveur !');
+//Charger les variables d'environnement du fichier .env
+require("dotenv").config();
+
+const normalizePort = (val) => {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+};
+
+const port = normalizePort(process.env.PORT || process.env.DB_HOST);
+app.set("port", port);
+
+const errorHandler = (error) => {
+    if (error.syscall !== "listen") {
+        throw error;
+    }
+    const address = server.address();
+    const bind =
+        typeof address === "string" ? "pipe " + address : "port: " + port;
+    switch (error.code) {
+        case "EACCES":
+            console.error(bind + " requires elevated privileges.");
+            process.exit(1);
+            break;
+        case "EADDRINUSE":
+            console.error(bind + " is already in use.");
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
+
+// Creation du server de l'application
+const server = http.createServer(app);
+
+server.on("error", errorHandler);
+server.on("listening", () => {
+    const address = server.address();
+    const bind =
+        typeof address === "string" ? "pipe " + address : "port " + port;
+    console.log("Listening on " + bind);
 });
 
-server.listen(process.env.PORT || 3000);
+server.listen(port);
